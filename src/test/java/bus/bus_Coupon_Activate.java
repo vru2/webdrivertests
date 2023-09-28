9ee1d695630f8d5ddcaba5cf25ab3cb8584b90df
Kiran Kumar <kiran.kumar@cleartrip.com>
2023-09-28 08:30:17 +0530

    PSE-569 Bus api fix

--- src/test/java/bus/bus_Coupon_Activate.java
+++ src/test/java/bus/bus_Coupon_Activate.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class bus_Coupon_Activate extends bus_Common_API {
 	
 	@Test
-	public void Bus_deactivateCoupon() throws Exception {
+	public void Bus_activateCoupon() throws Exception {
 		Response resp ;		
 		resp = busGet("couponActivate","");
 		validation("couponActivate", resp);
