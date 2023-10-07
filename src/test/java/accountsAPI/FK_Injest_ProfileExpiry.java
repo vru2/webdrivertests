88cc629a13185b8d1a1329323daa50d35fb2a829
saicharan <saicharan.yadau@cleartrip.com>
2023-10-07 23:56:53 +0530

    PSE-569 'FKVIP FIX'

---
+++ src/test/java/accountsAPI/FK_Injest_ProfileExpiry.java
@@ -0,0 +1,22 @@
+package test.java.  accountsAPI;
+
+import java.io.IOException;
+import org.json.JSONException;
+import org.testng.annotations.*;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+
+public class FK_Injest_ProfileExpiry extends AccountsCommon_API {
+
+    @Test
+    public void FK_Injest_ProfileExpiry()  throws IOException, JSONException {
+        Response resp ;
+        resp =postCall("FK_Injest_ProfileExpiry", "");
+        validation( resp, "FK_Injest_ProfileExpiry", "");
+
+		/*ResponseBody body = resp.getBody();
+		System.out.println("Response of API is:" + body.asString());*/
+
+
+    }
+}
